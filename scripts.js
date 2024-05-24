821d721ff02d07633b74ef7cdea7d75fdbade878
Luigtj <jeaneude33456@gmail.com>
2024-05-24 16:07:54 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let sections = document.querySelectorAll('section');
+    let currentSection = 0;
+
+    function scrollToSection(index) {
+        sections[index].scrollIntoView({ behavior: 'smooth' });
+        currentSection = index;
+    }
+
+    window.addEventListener('wheel', (event) => {
+        if (event.deltaY > 0 && currentSection < sections.length - 1) {
+            scrollToSection(currentSection + 1);
+        } else if (event.deltaY < 0 && currentSection > 0) {
+            scrollToSection(currentSection - 1);
+        }
+    });
+});
